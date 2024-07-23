c524320c01b92e56389f2045c38ad5c965a86318
Kushal Atul Ramaiya <kushalramaiya@gmail.com>
2024-07-23 13:10:32 +0530

    Created excel_csv.py
    Created an excel app

---
+++ excel_csv.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+# Title of the app
+st.title('Excel to CSV Converter')
+
+# Upload Excel file
+uploaded_file = st.file_uploader("Choose an Excel file", type="xls")
+
+if uploaded_file is not None:
+    # Read Excel file
+    df = pd.read_excel(uploaded_file,skiprows=6)
+
+    # Display the dataframe
+    st.write(df)
+
+    # Modify the dataframe here as per your requirement
+    # For example, let's assume we're dropping a column named 'Unnamed: 0'
+    if 'Unnamed: 0' in df.columns:
+        df = df.drop(columns=['Unnamed: 0'])
+    
+    df['Transaction Amount(INR)'] = df.apply(lambda row: -1 * row['Transaction Amount(INR)'] if row['Cr/Dr'] == 'DR' else row['Transaction Amount(INR)'], axis=1)
+    
+    # Write to CSV
+    csv = df.to_csv(index=False)
+    st.write("Modified CSV")
+    st.write(df)
+    # Download CSV file
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='modified_data.csv',
+        mime='text/csv',
+    )
